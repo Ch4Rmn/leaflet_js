c627b523bec6d7a51693194324a6b608c0cf230b
Lin Htut Kyaw <linhtut123451@gmail.com>
2025-07-07 16:01:50 +0630

    leaflet Js start with simple GeoJson

---
+++ js/your-geojson.js
@@ -0,0 +1,22 @@
+// js/your-geojson.js
+const geojsonData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": { "name": "Yangon" },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [96.1561, 16.8661]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Mandalay" },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [96.0836, 21.975]
+            }
+        }
+    ]
+};
